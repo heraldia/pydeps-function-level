fcd29bfda98f3b4b3af832144d639edf8611d563
Yunfei Feng <yunfei.feng@walmart.com>
2022-05-06 19:51:35 -0500

    [MODIFY] Readme.md(15): detect inner method | :2022_0506_1938

---
+++ Readme.md
@@ -0,0 +1,41 @@
+# Python Function Dependency graphs
+- Code Structure Reviewer 
+
+This is to plot dependency graphs at function level. [Python Function Dependency graphs](./)
+
+# Another sister project
+This is to plot dependency graphs at module/python-file level. 
+- [Python Module Dependency graphs](https://github.com/thebjorn/pydeps)
+
+# Setup
+```sh
+conda env update -f env/code_structure_review_env.yml
+conda activate code_structure_review_env
+```
+
+# Enhancement changlog
+- Jupyter Notebook
+- avoid functionName in comments
+- eliminating edge cases:
+    ```
+    def job_code(): # function-name exists in a plain string, but it should not taken as a function-call.
+    row , CAST(a.job_code AS INT) AS WORKGROUP_ID
+    ```
+- module name
+- class name
+
+- detect inner method and recognize function-call
+```py
+def wfp_check(loaded_model_3,loaded_fe_3):
+
+    share_wfp = loaded_model_3.rec_df[loaded_model_3.rec_df['STORE_NBR'].isin(pilot_stores)].merge(
+        loaded_model_3.cp_workgroup_dim_df,on='WORKGROUP_ID').merge(
+        loaded_model_3.cp_job_selection_dim_df,on='JOB_SELECTION_ID')
+    def formating(r):
+        pos = str(int(r['NUM_OF_POS']))
+```
+
+
+# Info
+Project starts at 2022_0413_1108  
+yunfei.feng@walmart.com
---
+++ codeStructureReivewer.py
@@ -0,0 +1,143 @@
+
+#target_dir = '/Users/y0f00k5/Documents/b/w/topK/driving-distance-Repos/DrivingDistanceAzureML/src/'
+#target_dir = '/Users/y0f00k5/Documents/b/w/ReqHelper/Hiring-Req-Helper-MLE-Service/'
+#target_dir = '/Users/y0f00k5/Documents/b/w/SCPH/scph-forecaster/src/'
+target_dir = '/Users/y0f00k5/Documents/b/w/ReqHelper/Hiring-Prompt-To-Hire/data_science/'
+
+import os
+import sys
+from collections import defaultdict
+import graphviz  # doctest: +NO_EXE
+import re
+import pysnooper
+
+function_set = set()
+class_set = set()
+
+def traverFilesInFolder(target_dir):
+    # get all classNames and functionNames
+    for root, dirs, files in os.walk(target_dir, topdown=False):
+        for fn in files:
+            if not fn.endswith('.py'):
+                continue
+            #print (os.path.join(root,fn))
+
+            if 'utils.py' not in os.path.join(root,fn):
+                continue
+
+            with open(os.path.join(root,fn), 'r', encoding='utf-8') as f:
+                for row in f:
+                    #print(sys._getframe().f_lineno,'| row',row  ); import pdb;pdb.set_trace() # 2022_0506_1927 
+
+                    if re.match("\s*def", row) or re.match("\s*class", row):
+                        if 'class' in row:
+                            t = row.replace('class ','').rstrip(":").strip()
+                            #class_set.add(os.path.join(root,fn)+":"+t)
+                            class_set.add(t)
+                        else:
+                            t = row.replace('def ','').strip()
+                            i = t.index('(') 
+
+                            function_set.add(os.path.join(root,fn).replace(target_dir, "")+"-"+t[:i])
+
+    print('class_set | ', class_set) # 2022_0413_1125
+    print('function_set | ', function_set) # 2022_0413_1125
+
+    # search calls for each className and functionName
+    function_callable_dic = defaultdict(list)
+    flag_in_comment_block = False
+
+
+    for root, dirs, files in os.walk(target_dir, topdown=False):
+        for fn in files:
+
+            if not fn.endswith('.py') and not fn.endswith('.ipynb'):
+                continue
+
+            if fn.endswith('.py'):
+                cur_class_name = 'None'
+                cur_function_name = fn
+            if fn.endswith('.ipynb'):
+                cur_class_name = 'None'
+                cur_function_name = fn
+
+            #print (os.path.join(root,fn))
+
+            with open(os.path.join(root,fn), 'r') as f:
+                flag_in_comment_block = False
+                for row in f:
+                    if re.search( "\s{4,}#",row) or row.startswith("#"): 
+                        continue
+                    if not flag_in_comment_block and row.startswith('"""'): 
+                        flag_in_comment_block = True
+                    elif flag_in_comment_block and row.startswith('"""'): 
+                        flag_in_comment_block = False
+
+                    if flag_in_comment_block:
+                        continue
+
+                    class_definition = False
+                    method_definition = False
+
+                    if re.match("\s*def", row) or re.match("\s*class" , row):
+                        if 'class' in row:
+                            cur_class_name = row.replace('class ','').rstrip(":").strip()
+                            cur_function_name = 'None'
+                            class_definition = True
+                            method_definition = False
+                        else:
+                            t = row.replace('def ','').strip()
+                            i = t.index('(') 
+                            cur_function_name = os.path.join(root,fn).replace(target_dir,"") + '-' + str(t[:i])
+                            method_definition = True
+                            class_definition = False
+                            #cur_function_name = str(t[:i])
+                            #cur_function_name = fn+"-"+str(t[:i])
+
+                    if method_definition:
+                        continue
+
+                    if '(' not in row:
+                        continue
+                        
+                    for _func in function_set:
+                        func = _func[_func.index('-')+1:]
+                        for func in [func+'(', func+',', func+' (', func+' ,']:
+                            if func in row:
+                                
+                                function_callable_dic.setdefault(cur_function_name, []).append(_func)
+
+    print(sys._getframe().f_lineno,'| function_callable_dic', function_callable_dic) # 2022_0413_1210
+
+    return function_callable_dic
+
+
+def render_function_callable_dic(function_callable_dic):
+
+    edge_set = set()
+    dot = graphviz.Digraph(comment=f'function calls: {target_dir}')
+
+    for k in function_callable_dic:
+        if 'root' in k or 'None' in k:
+            continue
+
+        if k.endswith('.py'):
+            dot.attr('node', shape='doublecircle')
+        else:
+            dot.attr('node', shape='rectangle')
+
+        dot.node(k)
+
+        dot.attr('node', shape='') 
+        for v in function_callable_dic[k]:
+            if (k,v) in edge_set:
+                continue
+            dot.node(v)
+            dot.edge(k, v)
+            edge_set.add((k,v))
+
+    dot.render('./function-call.gv').replace('\\', '/')
+
+function_callable_dic = traverFilesInFolder(target_dir)
+render_function_callable_dic(function_callable_dic)
+
